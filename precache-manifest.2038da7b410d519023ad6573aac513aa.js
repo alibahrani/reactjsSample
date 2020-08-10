89dfa1a355e41d5cf2b1723855ba5f8c2f653075
Ali Al-Bahrani <alibahrani@mac.com>
2020-08-10 08:39:55 -0400

    Updates

---
+++ precache-manifest.2038da7b410d519023ad6573aac513aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3784139e38022967cac73fd84a5a8401",
+    "url": "/alibahrani/reactjsSample/index.html"
+  },
+  {
+    "revision": "e63e109add9a3a5465ae",
+    "url": "/alibahrani/reactjsSample/static/css/main.0590d845.chunk.css"
+  },
+  {
+    "revision": "3f074525cee0f49a9956",
+    "url": "/alibahrani/reactjsSample/static/js/2.1e48545e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alibahrani/reactjsSample/static/js/2.1e48545e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e63e109add9a3a5465ae",
+    "url": "/alibahrani/reactjsSample/static/js/main.710cde40.chunk.js"
+  },
+  {
+    "revision": "906c9e6bb91a7c3435a0",
+    "url": "/alibahrani/reactjsSample/static/js/runtime-main.a776afa7.js"
+  }
+]);
